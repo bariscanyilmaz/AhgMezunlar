a95956f62eb2bdf0bde83ecf2d83cb9cd03a5bfd
ylmazbarscan <baris@bariscanyilmaz.com>
2018-06-29 13:26:30 +0300

    admin tablo görüntüle ikonları eklendi

--- AhgMezunlar/Controllers/EventsController.cs
+++ AhgMezunlar/Controllers/EventsController.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AhgMezunlar.Models.Abstract;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AhgMezunlar.Controllers
 {
     public class EventsController : Controller
     {
+        private IEventsRepository eventsRepository;
+
+        public EventsController(IEventsRepository _eventsRepository)
+        {
+            eventsRepository = _eventsRepository;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(eventsRepository.GetEvents());
         }
     }
 }
